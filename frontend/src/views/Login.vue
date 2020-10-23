a36fc36fdb536938c2551bf1fb63948a80c3c0a3
神田舞 <maiptomato@gmail.com>
2020-10-23 14:28:57 +0900

    ログインフォーム仮作成

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,11 +1,28 @@
 <template>
-<div>login page</div>
-
+  <div>
+    <div>login page</div>
+    <form method="post">
+      email<input type="email" v-model="email">
+      password<input type="text" v-model="password">
+      <button @click="login">Login</button>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: "Login"
+  name: "Login",
+  data () {
+    return {
+      email: "",
+      password: ""
+    }
+  },
+  methods: {
+    login() {
+      console.log(this.email)
+    }
+  }
 }
 </script>
 
