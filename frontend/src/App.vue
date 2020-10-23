a5432db965a9fd46a01f8457273baddc98a34eca
神田舞 <maiptomato@gmail.com>
2020-10-23 14:07:50 +0900

    管理画面ログインルートの作成

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,10 @@
 <template>
   <div id="app">
+    <div>
+      <button>
+        <router-link to="/login">Admin</router-link>
+      </button>
+    </div>
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
