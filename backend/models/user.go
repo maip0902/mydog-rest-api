d6971f5e7a34b3b49344205fb2fd911f0fda208e
神田舞 <maiptomato@gmail.com>
2020-11-05 21:22:47 +0900

    パスワードのバリデーション

--- backend/models/user.go
+++ backend/models/user.go
@@ -5,6 +5,7 @@ import (
     "github.com/go-ozzo/ozzo-validation/v4/is"
     "github.com/globalsign/mgo/bson"
     "time"
+    "regexp"
 )
 
 type User struct {
@@ -16,10 +17,20 @@ type User struct {
     VerifiedAt time.Time `bson:"verified_at"`
 }
 
+// func (user *User) CreateUserValidate() error {
+//     email := &user.Email
+//     return validation.Validate(email,
+//         validation.Required.Error("メールアドレスは必須です"),
+//         is.Email.Error("正しいメールアドレスの形で入力してください"),
+//     )
+// }
+
 func (user *User) CreateUserValidate() error {
-    email := &user.Email
-    return validation.Validate(email,
-        validation.Required.Error("メールアドレスは必須です"),
-        is.Email.Error("正しいメールアドレスの形で入力してください"),
+    return validation.ValidateStruct(user,
+        validation.Field(&user.Email, validation.Required.Error("メールアドレスは必須です")),
+        validation.Field(&user.Email, is.Email.Error("正しいメールアドレスの形で入力してください")),
+        validation.Field(&user.Password, validation.Required.Error("パスワードは必須です")),
+        validation.Field(&user.Password, validation.Match(regexp.MustCompile("^([a-zA-Z0-9]{8,})$")).Error("パスワードは半角英数8文字以上です")),
+        validation.Field(&user.Password, validation.Length(8, 20).Error("パスワードは半角英数8文字以上です")),
     )
 }
