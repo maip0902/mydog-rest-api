dc61832d85db43173a84ae2ce8e746f9a85dcd0a
神田舞 <maiptomato@gmail.com>
2020-11-07 10:55:45 +0900

    ログインとログアウトのじっそう

--- frontend/src/views/Top.vue
+++ frontend/src/views/Top.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
     Top
+    <button @click="logout">
+      logout
+    </button>
   </div>
 </template>
 
 <script>
 export default {
-name: "Top"
+name: "Top",
+  beforeCreate() {
+  console.log(this.$session.exists())
+    if (!this.$session.exists()) {
+      this.$router.push('/signUp')
+    }
+  },
+  methods: {
+    logout() {
+      this.$session.destroy()
+      this.$router.push('/signUp')
+    }
+  }
 }
 </script>
 
