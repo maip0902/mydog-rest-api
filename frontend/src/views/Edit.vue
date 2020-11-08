6950ef5559d2cb962a74163505522013ee285e1c
神田舞 <maiptomato@gmail.com>
2020-11-08 23:21:39 +0900

    編集ページ実装

--- frontend/src/views/Edit.vue
+++ frontend/src/views/Edit.vue
@@ -1,16 +1,32 @@
 <template>
-<div>edit</div>
+  <div>
+    <div>code : {{ code }}</div>
+    <div>
+      Description <input type="textarea" v-model="description">
+    </div>
+    <button>編集する</button>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
 export default {
 name: "Edit",
+  data () {
+    return {
+     code: "",
+     description: "",
+     image: ""
+    }
+  },
   created() {
   console.log(this.$route.params.id)
     axios.get('http://localhost:3000/codeImage/' + this.$route.params.id)
       .then((res) => {
         console.log(res.data)
+        this.code = res.data.Code
+        this.description = res.data.Description
+        this.image = res.data.Image
       })
   }
 }
