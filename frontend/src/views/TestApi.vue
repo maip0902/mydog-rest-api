9a95687fa69f0de8653e3e7599fc3856a580c051
神田舞 <maiptomato@gmail.com>
2020-11-23 17:25:02 +0900

    画像も表示する

--- frontend/src/views/TestApi.vue
+++ frontend/src/views/TestApi.vue
@@ -2,6 +2,9 @@
   <div>
       <input type="text" v-model="statusCode">
       <button @click="get">Click!</button>
+      <div>
+          <img :src=image class="top_img"> 
+      </div>
       <dt>{{description}}</dt>
   </div>
 </template>
@@ -15,6 +18,7 @@ export default {
       return {
           "statusCode": "",
           "description": "",
+          image : ""
       }
   },
   methods: {
@@ -23,6 +27,9 @@ export default {
             .then((res) => {
                 console.log(res.data)
                 this.description = res.data.Description
+                console.log(this.statusCode)
+                this.image = require("../assets/" + String(this.statusCode) + ".jpg")
+                console.log(this.image)
             })
             .catch((err) => {
                 console.log(err.response)
