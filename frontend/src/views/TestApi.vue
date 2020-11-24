7026383cd68f519889911b8fa5fd23a5dab18d5b
神田舞 <maiptomato@gmail.com>
2020-11-24 11:30:27 +0900

    テスト用の見た目の修正

--- frontend/src/views/TestApi.vue
+++ frontend/src/views/TestApi.vue
@@ -1,11 +1,13 @@
 <template>
-  <div>
-      <input type="text" v-model="statusCode">
-      <button @click="get">Click!</button>
-      <div>
-          <img :src=image class="top_img"> 
+  <div class="main-content">
+      <input type="text" v-model="statusCode" class="main-input">
+      <button @click="get" class="main-button">Click!</button>
+      <div class="box-image">
+          <img :src=image class="list-image"> 
       </div>
-      <dt>{{description}}</dt>
+    <div class="main-box">
+      <div class="main-description">{{description}}</div>
+    </div>
   </div>
 </template>
 
@@ -39,5 +41,50 @@ export default {
 </script>
 
 <style scoped>
+.main-content
+{
+    width: 100%;
+    margin-left: auto;
+    margin-right: auto;
+    margin-top: 5px;
+}
 
+.box-image
+{
+    margin-right: 40px;
+}
+
+.list-image 
+{
+  width: 150px;
+  height: 150px;
+}
+
+.main-input
+{
+    width: 150px;
+    height: 30px;
+    border-radius: 8px;
+    border:3px solid #FFCCCC;
+}
+
+.main-button 
+{
+    height: 35px;
+    border-radius: 8px;
+    border:3px solid #FF9966;
+    background-color: #FF9966;
+    color: #FFFFFF
+}
+
+.main-box 
+{
+    margin-right: 40px;
+}
+
+.main-description
+{
+    font-weight: bold;
+    color: #FF9966;
+}
 </style>
