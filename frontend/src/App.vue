4d0d6f0c35ed702248c63790c10c43731193d84d
神田舞 <maiptomato@gmail.com>
2020-12-11 23:55:34 +0900

    scssを読み込ませる

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -12,37 +12,6 @@
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-.top_header {
-    width: 100%;
-    height: 50px;
-    background-color:darksalmon;
-}
-
-.header_app_name {
-  float: left;
-  margin-top: 10px;
-}
-
-.header_app_name a {
-  font-weight: bold;
-  text-decoration: none;  
-  font-size: 24px;
-  color: darkslateblue
-}
-
-.header_button {
-  float: right;
-  margin-top: 13px;
-}
-
-
+<style lang="scss">
+@import "../sass/top.scss";
 </style>
