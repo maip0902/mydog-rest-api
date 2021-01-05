f4f70a137af8a0f115e47b014af583bd0e0407a0
神田舞 <maiptomato@gmail.com>
2021-01-05 22:08:31 +0900

    新規会員登録ぺーじの修正

--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -1,10 +1,24 @@
 <template>
   <div>
     {{ error }}
-    <div>Sign up</div>
-      email<input type="email" v-model="email">
-      password<input type="password" v-model="password">
-      <button @click="signup">register</button>
+    <div class="login-input">
+      <label for="email" class="login-label">メールアドレス</label>
+      <input id="email" type="email" class="login-field" v-model="email">
+    </div>
+    
+    <div class="login-input">
+      <label for="password" class="login-label">パスワード</label>
+      <input id="password" type="text" class="login-field" v-model="password">
+    </div>
+
+    <div class="login-input">
+      <label for="password_confirm" class="login-label">パスワード(確認用)</label>
+      <input id="password_confirm" type="text" class="login-field" v-model="passwordConfirm">
+    </div>
+    
+    <div class="btn-field">
+      <button id="loginBtn" class="main-btn login-btn" @click="signup">新規会員登録</button>
+    </div>  
   </div>
 </template>
 
@@ -17,17 +31,21 @@ export default {
     return {
       email: "",
       password: "",
+      passwordConfirm: "",
       error: ""
     }
   },
   methods: {
     signup() {
-      console.log(this.email)
+      if(this.password !== this.passwordConfirm) {
+        this.error = "パスワードは確認用と一致しません"
+        return
+      }
       let params = JSON.stringify({
         password: this.password,
         email: this.email
       })
-      axios.post("http://localhost:3000/signUp", params, {headers: {'Content-Type': 'application/json','Accept': 'application/json'}})
+      axios.post("http://localhost/api/signUp", params, {headers: {'Content-Type': 'application/json','Accept': 'application/json'}})
         .then( res => {
           try {
             console.log(res.data.Token)
